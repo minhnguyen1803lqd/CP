4f6747262dcbab86c1520a69c48cd5c1d1bdd26d
minhnguyen1803lqd <nhatminh1803lqd@gmail.com>
2020-05-21 00:21:37 +0700

    testing git auto

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ import sys
 os.system("git add --all")
 os.system("git commit -m \"testing git auto\"")
 os.system("git push origin master")
-os.system("printf \'minhnguyen1803lqd\'")
-os.system("printf \'nguyenhuonggiang4811\'")
+os.system("printf \'minhnguyen1803lqd\n\'")
+os.system("printf \'nguyenhuonggiang4811\n\'")
