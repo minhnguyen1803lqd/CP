ca14e3380f9544b19d2ee42925477bb0e3ea9d9a
minhnguyen1803lqd <nhatminh1803lqd@gmail.com>
2020-06-05 14:07:59 +0700

    update new problems

--- codeforces/VNOI/vmunch/vmunch.cpp
+++ codeforces/VNOI/vmunch/vmunch.cpp
@@ -17,9 +17,41 @@ using namespace std;
 
 const int inf = 1e9 + 7;
 const int esf = 1e-9;
+const int N = 1e4 + 7;
+
+int n, m;
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
+void BFS(ii source) {
+    queue < ii > q;
+    int color[N][N];
+    memset(color, 0, sizeof(color));
+    q.push(source);
+    while (!q.empty()) {
+        ii u = q.front();
+        q.pop();
+        REP(k, 4) {
+            if (!color[u.fi + dx[k]][u.se + dy[k]]) {
+                
+            }
+        }
+    }
+}
 
 int main() {
-    fileInput("");
+    fileInput("vmunch");
     fast;
+    cin >> n >> m;
+    FOR(i, 1, n) {
+        FOR(j, 1, m) {
+            chat tmp;
+            cin >> tmp;
+            if (tmp == 'B') source = ii(i, j);
+            if (tmp == 'C') sink = ii(i, j);
+            if (tmp == '*') color[i][j] = 1;
+        }
+    }
+    BFS(source);
     return (0);
 }
